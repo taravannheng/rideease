8ca6927c7c95fdf42d86e503a8c2bec534e47784
taravannheng <heng.t.taravann@gmail.com>
2022-12-23 11:32:26 +1100

    feat(route): add route constants

---
+++ src/utils/constants/routes.tsx
@@ -0,0 +1,7 @@
+export const LANDING = '/'
+export const SIGNUP = '/signup'
+export const SIGNIN = '/signin'
+export const HOME = '/home'
+export const BOOK = '/book'
+export const PAYMENT = '/payment'
+export const CONFIRMATION = '/confirmation'
