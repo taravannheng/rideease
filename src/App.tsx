59f8aecd8e1847f2e273b50b9e8a41be8f409ea2
taravannheng <heng.t.taravann@gmail.com>
2022-12-23 18:04:16 +1100

    feat(route): add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { FC } from 'react'
+import React, { FC, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as ROUTES from "./utils/constants/routes";
 
 import LandingPage from "./routes/landing/index.component";
 
+// LAZY LOADING
+const SignUpPage = React.lazy(() => import("./routes/signup/index.component"));
+
 const App: FC = () => {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path={ROUTES.LANDING} element={<LandingPage />} />
+        <Route
+          path={ROUTES.SIGNUP}
+          element={
+            <Suspense fallback={<>...</>}>
+              <SignUpPage />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
