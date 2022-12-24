b48220f3d481322405b8b3f68a4895cd82cd4f9e
taravannheng <heng.t.taravann@gmail.com>
2022-12-25 08:53:38 +1100

    refactor(component): change accordion item appearance

--- src/components/accordion/index.component.tsx
+++ src/components/accordion/index.component.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { v4 as uuidv4 } from 'uuid';
 
 interface AccordionProps {
@@ -23,8 +23,8 @@ const Accordion: FC<AccordionProps> = ({ items, className }) => {
   return (
     <ul className={`accordion ${className}`}>
       {items.map((item, index) => (
-        <li key={item.id} className={`transition border-[1px] border-neutral-grey-2 overflow-hidden mb-2 p-2 cursor-pointer ${expandedItem === index ? 'h-auto' : 'h-10'}`}>
-          <h4 onClick={() => toggle(index)} className="text-h4 text-neutral-dark hover:text-primary flex flex-row justify-between items-center mb-2">{item.title} <FontAwesomeIcon icon={expandedItem === index ? faMinus : faPlus} /></h4>
+        <li key={item.id} className={`transition border-b-[1px] border-neutral-grey-2 overflow-hidden mb-2 p-2 cursor-pointer ${expandedItem === index ? 'h-auto' : 'h-10'}`}>
+          <h4 onClick={() => toggle(index)} className="text-h4 text-neutral-dark hover:text-primary flex flex-row justify-between items-center mb-2">{item.title} <FontAwesomeIcon icon={expandedItem === index ? faChevronUp : faChevronDown} /></h4>
           {
             item.innerItems.map(innerItem => (
               <p key={uuidv4()} className="text-body text-neutral-grey-4 mt-2">{innerItem}</p>
