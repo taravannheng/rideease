d05cd58c98eb248b8ac26643f5f8ecdb079a8b96
taravannheng <heng.t.taravann@gmail.com>
2022-12-24 18:44:16 +1100

    feat(validator): add function to check matching password

--- src/utils/validators/validator.ts
+++ src/utils/validators/validator.ts
@@ -3,7 +3,7 @@ export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-
 export const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
 export const emptyRegex = /([^\s]*)/;
-//https://stackoverflow.com/questions/2249460/how-to-use-javascript-regex-to-check-for-empty-form-fields
+// https://stackoverflow.com/questions/2249460/how-to-use-javascript-regex-to-check-for-empty-form-fields
 
 export const checkEmail = (value: string) => {
   return emailRegex.test(String(value).toLowerCase());
@@ -15,4 +15,8 @@ export const checkPassword = (value: string) => {
 
 export const checkEmpty = (value: string) => {
   return emptyRegex.test(value);
+}
+
+export const checkMatchedPassword = (password1: string, password2: string) => {
+  return password1 === password2;
 }
