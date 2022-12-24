970ed200fbcd3a0a4b4f50638feab92d3a4746da
taravannheng <heng.t.taravann@gmail.com>
2022-12-24 19:03:28 +1100

    feat(component): add link to button for redirecting

--- src/components/button/index.component.tsx
+++ src/components/button/index.component.tsx
@@ -1,37 +1,71 @@
 import { FC } from "react";
-
+import { Link } from "react-router-dom";
 interface ButtonProps {
   type: "button" | "submit";
   buttonStyle: "primary" | "secondary" | "text";
   className?: string;
   children: string;
   imageSource?: string;
+  route?: string;
 }
 
-const Button: FC<ButtonProps> = ({ type, buttonStyle, children, className, imageSource }) => {
+const Button: FC<ButtonProps> = ({
+  type,
+  buttonStyle,
+  children,
+  className,
+  imageSource,
+  route,
+}) => {
   let appearance: string;
 
-  switch(buttonStyle) {
-    case 'primary': 
-      appearance = "bg-primary text-neutral-light hover:bg-primary-dark"
+  switch (buttonStyle) {
+    case "primary":
+      appearance = "bg-primary text-neutral-light hover:bg-primary-dark";
       break;
-    case 'secondary':
-      appearance = "bg-neutral-grey-1 text-neutral-dark hover:bg-primary hover:text-neutral-light"
+    case "secondary":
+      appearance =
+        "bg-neutral-grey-1 text-neutral-dark hover:bg-primary hover:text-neutral-light";
       break;
-    case 'text':
-      appearance = "bg-none text-neutral-dark hover:text-primary"
+    case "text":
+      appearance = "bg-none text-neutral-dark hover:text-primary";
       break;
     default:
-      appearance = "bg-primary text-neutral-light hover:bg-primary-dark"
+      appearance = "bg-primary text-neutral-light hover:bg-primary-dark";
   }
 
   return (
-    <button
-      type={type}
-      className={`${appearance} text-body  border-none container cursor-pointer transition ${className} min-w-full flex flex-row justify-center items-center py-3 px-3`}
-    >
-      {imageSource && <img src={imageSource} alt="button logo" className="w-6 h-6 mr-2" />} {children}
-    </button>
+    <>
+      {route && (
+        <Link to={route}>
+          <button
+            type={type}
+            className={`${appearance} text-body  border-none container cursor-pointer transition ${className} min-w-full flex flex-row justify-center items-center py-3 px-3`}
+          >
+            {imageSource && (
+              <img
+                src={imageSource}
+                alt="button logo"
+                className="w-6 h-6 mr-2"
+              />
+            )}{" "}
+            {children}
+          </button>
+        </Link>
+      )}
+
+      {!route && (
+        <button
+          type={type}
+          className={`${appearance} text-body  border-none container cursor-pointer transition ${className} min-w-full flex flex-row justify-center items-center py-3 px-3`}
+        >
+          {imageSource && (
+            <img src={imageSource} alt="button logo" className="w-6 h-6 mr-2" />
+          )}{" "}
+          {children}
+        </button>
+      )}
+    </>
   );
 };
 
