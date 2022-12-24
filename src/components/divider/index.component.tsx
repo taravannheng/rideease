de90011d7ed0b48e429e0ac376f665e252c210a2
taravannheng <heng.t.taravann@gmail.com>
2022-12-24 21:45:24 +1100

    refactor(component): change font size for divider component

--- src/components/divider/index.component.tsx
+++ src/components/divider/index.component.tsx
@@ -16,7 +16,7 @@ const Divider: FC<DividerProps> = ({ className = '', children = '' }) => {
     {checkEmpty(children) && (
       <div className={`flex flex-row items-center justify-center ${className}`}>
         <div className={`h-[1px] bg-neutral-grey-2 w-full`} />
-        <span className="text-neutral-grey-4 text-body bg-neutral-light px-2 py-1">{children}</span>
+        <span className="text-neutral-grey-4 text-sub1 bg-neutral-light px-2 py-1">{children}</span>
         <div className={`h-[1px] bg-neutral-grey-2 w-full`} />
       </div>
     )}
