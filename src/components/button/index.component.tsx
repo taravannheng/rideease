1ad510ab34f6b214d7f6bd4a4f2c3abc0c6dc605
taravannheng <heng.t.taravann@gmail.com>
2022-12-24 18:16:41 +1100

    fix(component): fix button width not extending to full on larger screens

--- src/components/button/index.component.tsx
+++ src/components/button/index.component.tsx
@@ -28,7 +28,7 @@ const Button: FC<ButtonProps> = ({ type, buttonStyle, children, className, image
   return (
     <button
       type={type}
-      className={`${appearance} text-body w-full py-3 px-3 border-none container cursor-pointer transition flex flex-row justify-center items-center ${className}`}
+      className={`${appearance} text-body  border-none container cursor-pointer transition ${className} min-w-full flex flex-row justify-center items-center py-3 px-3`}
     >
       {imageSource && <img src={imageSource} alt="button logo" className="w-6 h-6 mr-2" />} {children}
     </button>
