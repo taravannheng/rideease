f6c6644547412cc3fa4b81acbf12ea9fee080f5c
taravannheng <heng.t.taravann@gmail.com>
2022-12-28 23:41:39 +1100

    build(dependency): add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { FC, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import * as ROUTES from "./utils/constants/routes";
+import { initializeApp } from "firebase/app";
 
 import LandingPage from "./routes/landing/index.component";
+import * as ROUTES from "./utils/constants/routes";
+import firebaseConfig from "./utils/firebase/firebase-config";
 
 // LAZY LOADING
 const SignUpPage = React.lazy(() => import("./routes/signup/index.component"));
+const SignInPage = React.lazy(() => import("./routes/signin/index.component"));
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const App: FC = () => {
   return (
@@ -17,6 +23,14 @@ const App: FC = () => {
           element={
             <Suspense fallback={<>...</>}>
               <SignUpPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path={ROUTES.SIGNIN}
+          element={
+            <Suspense fallback={<>...</>}>
+              <SignInPage />
             </Suspense>
           }
         />
