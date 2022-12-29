008c9b702ab468125194305c5369234b4bdb0af8
taravannheng <heng.t.taravann@gmail.com>
2022-12-29 18:42:50 +1100

    feat(context): add cart context

---
+++ src/contexts/cart-context.js
@@ -0,0 +1,17 @@
+import { useState, createContext } from "react"
+
+const CartContext = createContext()
+
+const CartContextProvider = ({ children }) => {
+  const [cartState, setCartState] = useState([])
+
+  return <CartContext.Provider value={{
+    cartState,
+    setCartState
+  }}>
+    {children}
+  </CartContext.Provider>
+}
+
+export {CartContextProvider}
+export default CartContext
