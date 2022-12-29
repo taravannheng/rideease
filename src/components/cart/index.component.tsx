3ebe48c4eeebc00a70743391c2d54606417024c0
taravannheng <heng.t.taravann@gmail.com>
2022-12-29 13:29:47 +1100

    feat(component): add icon for displaying cart on small screens

--- src/components/cart/index.component.tsx
+++ src/components/cart/index.component.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 // todo: add prop for redirecting to cart page
 
 const Cart: FC = () => {
   return (
-    <div className="cart h-12 w-40 bg-primary hover:bg-primary-dark transition border-none text-neutral-light text-body flex items-center justify-between px-6 cursor-pointer">
-      <span>Reservations</span>
+    <div className="cart h-12 bg-primary hover:bg-primary-dark transition border-none text-neutral-light text-body flex items-center justify-between px-6 gap-x-4 cursor-pointer">
+      <span className="hidden sm:inline">Reservations</span>
+      <FontAwesomeIcon icon={faCartShopping} className="inline sm:hidden w-[24px] h-[24px]" />
       <div className="cart__count-container w-6 h-6 bg-neutral-light flex items-center justify-center rounded-full">
         <span className="cart__count text-neutral-dark text-body">0</span>
       </div>
