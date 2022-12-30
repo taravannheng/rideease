de1a90b556f3b5edc57029a3fe7887ab96c8c341
taravannheng <heng.t.taravann@gmail.com>
2022-12-30 20:04:34 +1100

    feat(component): prevent redirect on cart component if cart is empty

--- src/components/cart/index.component.tsx
+++ src/components/cart/index.component.tsx
@@ -1,16 +1,17 @@
 import { FC, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import CartContext from "../../contexts/cart-context";
 import * as ROUTES from "../../utils/constants/routes";
 
 const Cart: FC = () => {
   const { cartState } = useContext(CartContext);
+  const location = useLocation();
 
   return (
-    <Link to={ROUTES.CART}>
+    <Link to={cartState.length > 0 ? ROUTES.CART : location.pathname}>
       <div className="cart h-12 bg-primary hover:bg-primary-dark transition border-none text-neutral-light text-body flex items-center justify-between px-6 gap-x-4 cursor-pointer">
         <FontAwesomeIcon icon={faCartShopping} className="w-[24px] h-[24px]" />
         <div className="cart__count-container w-6 h-6 bg-neutral-light flex items-center justify-center rounded-full">
