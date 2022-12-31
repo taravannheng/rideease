a954bb258b0139a4f617596087a2721b9c916ec6
taravannheng <heng.t.taravann@gmail.com>
2023-01-01 00:14:48 +1100

    feat(route): add route to successful payment page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { FC, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { initializeApp } from "firebase/app";
+import "@stripe/stripe-js";
 
 import LandingPage from "./routes/landing/index.component";
 import * as ROUTES from "./utils/constants/routes";
 import firebaseConfig from "./utils/firebase/firebase-config";
 import { UserContextProvider } from "./contexts/user-context";
 import { CartContextProvider } from "./contexts/cart-context";
+import { ProductContextProvider } from "./contexts/product-context";
 
 // LAZY LOADING
 const SignUpPage = React.lazy(() => import("./routes/signup/index.component"));
@@ -15,62 +17,77 @@ const BookingPage = React.lazy(
   () => import("./routes/booking/index.component")
 );
 const CartPage = React.lazy(() => import("./routes/cart/index.component"));
-const NotFoundPage = React.lazy(() => import("./routes/notfound/index.component"));
+const NotFoundPage = React.lazy(
+  () => import("./routes/notfound/index.component")
+);
+const PaymentSuccessPage = React.lazy(
+  () => import("./routes/payment-success/index.component")
+);
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
 const App: FC = () => {
   return (
-    <UserContextProvider>
-      <CartContextProvider>
-        <Router>
-          <Routes>
-            <Route path={ROUTES.HOME} element={<LandingPage />} />
-            <Route
-              path={ROUTES.SIGNUP}
-              element={
-                <Suspense fallback={<>...</>}>
-                  <SignUpPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path={ROUTES.SIGNIN}
-              element={
-                <Suspense fallback={<>...</>}>
-                  <SignInPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path={ROUTES.BOOKING}
-              element={
-                <Suspense fallback={<>...</>}>
-                  <BookingPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path={ROUTES.CART}
-              element={
-                <Suspense fallback={<>...</>}>
-                  <CartPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path={ROUTES.NOTFOUND}
-              element={
-                <Suspense fallback={<>...</>}>
-                  <NotFoundPage />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </Router>
-      </CartContextProvider>
-    </UserContextProvider>
+    <ProductContextProvider>
+      <UserContextProvider>
+        <CartContextProvider>
+          <Router>
+            <Routes>
+              <Route path={ROUTES.HOME} element={<LandingPage />} />
+              <Route
+                path={ROUTES.SIGNUP}
+                element={
+                  <Suspense fallback={<>...</>}>
+                    <SignUpPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={ROUTES.SIGNIN}
+                element={
+                  <Suspense fallback={<>...</>}>
+                    <SignInPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={ROUTES.BOOKING}
+                element={
+                  <Suspense fallback={<>...</>}>
+                    <BookingPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={ROUTES.CART}
+                element={
+                  <Suspense fallback={<>...</>}>
+                    <CartPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={ROUTES.NOTFOUND}
+                element={
+                  <Suspense fallback={<>...</>}>
+                    <NotFoundPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path={ROUTES.PAYMENT_SUCCESS}
+                element={
+                  <Suspense fallback={<>...</>}>
+                    <PaymentSuccessPage />
+                  </Suspense>
+                }
+              />
+            </Routes>
+          </Router>
+        </CartContextProvider>
+      </UserContextProvider>
+    </ProductContextProvider>
   );
 };
 
---
+++ src/routes/payment-success/index.component.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+const PaymentSuccessPage: FC = () => {
+  return (
+    <div>Successful Payment</div>
+  )
+}
+
+export default PaymentSuccessPage
