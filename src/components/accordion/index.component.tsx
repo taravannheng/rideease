c9a114b4caf8094726b686415e86d7fa6f117392
taravannheng <heng.t.taravann@gmail.com>
2023-01-02 00:39:34 +1100

    feat(component): add animation to accordion

--- src/components/accordion/index.component.tsx
+++ src/components/accordion/index.component.tsx
@@ -1,36 +1,54 @@
 import { FC, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
-import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
+import { motion } from "framer-motion";
 
 interface AccordionProps {
-  items: { id: string, title: string; innerItems: string[] }[];
+  items: { id: string; title: string; innerItems: string[] }[];
   className?: string;
 }
 
 const Accordion: FC<AccordionProps> = ({ items, className }) => {
-  const [expandedItem, setExpandedItem] = useState<null | number>(null)
+  const [expandedItem, setExpandedItem] = useState<null | number>(null);
 
   const toggle = (index: number) => {
     if (expandedItem === index) {
-      setExpandedItem(null)
-      return ;
+      setExpandedItem(null);
+      return;
     }
 
     setExpandedItem(index);
-  }
+  };
 
   return (
     <ul className={`accordion ${className}`}>
       {items.map((item, index) => (
-        <li key={item.id} className={`transition border-b-[1px] border-neutral-grey-2 overflow-hidden mb-2 p-2 cursor-pointer ${expandedItem === index ? 'h-auto' : 'h-10'}`}>
-          <h4 onClick={() => toggle(index)} className="text-h4 text-neutral-dark hover:text-primary flex flex-row justify-between items-center mb-2">{item.title} <FontAwesomeIcon icon={expandedItem === index ? faChevronUp : faChevronDown} /></h4>
-          {
-            item.innerItems.map(innerItem => (
-              <p key={uuidv4()} className="text-body text-neutral-grey-4 mt-2">{innerItem}</p>
-            ))
-          }
-        </li>
+        <motion.li
+          layout
+          transition={{
+            opacity: { ease: "linear" },
+            layout: { duration: 0.15 },
+          }}
+          style={{ height: `${expandedItem === index ? "auto" : "2.5rem"}` }}
+          key={item.id}
+          className={`border-b-[1px] border-neutral-grey-2 overflow-hidden mb-2 p-2 cursor-pointer`}
+        >
+          <h4
+            onClick={() => toggle(index)}
+            className="text-h4 text-neutral-dark hover:text-primary flex flex-row justify-between items-center mb-2"
+          >
+            {item.title}{" "}
+            <FontAwesomeIcon
+              icon={expandedItem === index ? faChevronUp : faChevronDown}
+            />
+          </h4>
+          {item.innerItems.map((innerItem) => (
+            <p key={uuidv4()} className="text-body text-neutral-grey-4 mt-2">
+              {innerItem}
+            </p>
+          ))}
+        </motion.li>
       ))}
     </ul>
   );
