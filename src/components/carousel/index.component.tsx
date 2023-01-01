9f0e45f3d93210166cf7cbda23fdad71be65d5df
taravannheng <heng.t.taravann@gmail.com>
2023-01-01 21:14:22 +1100

    feat(component): add cart state to local storage

--- src/components/carousel/index.component.tsx
+++ src/components/carousel/index.component.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useContext } from "react";
+import { FC, useState, useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
@@ -15,7 +15,13 @@ interface CarouselProps {
   showBookButton?: boolean;
 }
 
-const Carousel: FC<CarouselProps> = ({ items, showDetails = true, showBookButton = true }) => {
+const Carousel: FC<CarouselProps> = ({
+  items,
+  showDetails = true,
+  showBookButton = true,
+}) => {
+  const lsCartState = localStorage.getItem('ls-cart-state');
+
   // CONTEXTS
   const { cartState, setCartState } = useContext(CartContext);
 
@@ -39,11 +45,12 @@ const Carousel: FC<CarouselProps> = ({ items, showDetails = true, showBookButton
     if (
       cartState.find((item: CartItemModel) => item.id === items[activeItem].id)
     ) {
-      setCartState(
-        cartState.filter(
-          (item: CartItemModel) => item.id !== items[activeItem].id
-        )
+      const filteredCart = cartState.filter(
+        (item: CartItemModel) => item.id !== items[activeItem].id
       );
+
+      setCartState(filteredCart);
+      localStorage.setItem("ls-cart-state", JSON.stringify(filteredCart));
       return;
     }
 
@@ -51,99 +58,108 @@ const Carousel: FC<CarouselProps> = ({ items, showDetails = true, showBookButton
       ...prevCartState,
       items[activeItem],
     ]);
+
+    localStorage.setItem(
+      "ls-cart-state",
+      JSON.stringify([...cartState, items[activeItem]])
+    );
   };
 
+  useEffect(() => {
+    setCartState(JSON.parse(lsCartState!));
+  },[]);
+
   return (
-      <div className="carousel relative w-full">
-        <div
-          className={`${
-            activeItem === 0 && "hidden"
-          } carousel__left-icon w-10 h-10 absolute flex justify-center items-center bg-neutral-grey-1 rounded-full left-5 sm:left-10 lg:left-40 top-28 lg:top-48 z-10`}
-          onClick={carouselLeftIconHandler}
-        >
-          <FontAwesomeIcon
-            icon={faChevronLeft}
-            className="w-5 h-5 text-neutral-dark cursor-pointer hover:text-primary"
-          />
-        </div>
-        <ul
-          className={`landing__car-preview-slides flex flex-row w-full mt-10 transition`}
-          style={{ transform: `translate(-${activeItem * 100}%, 0px)` }}
-        >
-          {items.map((item, index) => (
-            <li
-              key={item.id}
-              className={`carousel-item-${index} min-w-full flex flex-col items-center`}
-            >
-              <figure>
-                <img
-                  src={item.imgSrc}
-                  alt={item.alt}
-                  className={`max-h-60 lg:h-96 lg:max-h-96 ${item.className}`}
-                />
-                <figcaption className="text-center text-neutral-grey-4">
-                  {item.name}
-                </figcaption>
-              </figure>
-              {showDetails && (
-                <div className="bg-neutral-grey-1 flex flex-col w-2/3 gap-y-2 md:gap-y-0 items-start md:flex-row md:w-[560px] p-4 md:p-8 md:justify-between md:items-center mt-8">
-                  <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
-                    <p className="text-neutral-grey-4 text-sub1">Category</p>
-                    <p className="text-neutral-dark">{item.details.category}</p>
-                  </div>
-                  <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
-                    <p className="text-neutral-grey-4 text-sub1">Mileage</p>
-                    <p className="text-neutral-dark">{item.details.mileage.toLocaleString()} kms</p>
-                  </div>
-                  <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
-                    <p className="text-neutral-grey-4 text-sub1">Fuel Type</p>
-                    <p className="text-neutral-dark">{item.details.fuelType}</p>
-                  </div>
-                  <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
-                    <p className="text-neutral-grey-4 text-sub1">Seats</p>
-                    <p className="text-neutral-dark">{item.details.seats}</p>
-                  </div>
-                  <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
-                    <p className="text-neutral-grey-4 text-sub1">
-                      Price Per Day
-                    </p>
-                    <p className="text-neutral-dark">
-                      ${item.details.pricePerDay}
-                    </p>
-                  </div>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-        {showBookButton && (
-          <div className="booking__book-button flex items-center justify-center mt-16">
-            <Button
-              className="min-w-[8rem] w-32 max-w-lg"
-              buttonStyle="primary"
-              type="button"
-              onClick={cartHandler}
-            >
-              {cartState.find(
-                (item: CartItemModel) => item.id === items[activeItem].id
-              )
-                ? "Added to Cart"
-                : "Book Now"}
-            </Button>
-          </div>
-        )}
-        <div
-          className={`${
-            activeItem === items.length - 1 && "hidden"
-          } carousel__right-icon w-10 h-10 absolute flex justify-center items-center bg-neutral-grey-1 rounded-full right-5 sm:right-10 lg:right-40 top-28 lg:top-48 z-10`}
-          onClick={carouselRightIconHandler}
-        >
-          <FontAwesomeIcon
-            icon={faChevronRight}
-            className="w-5 h-5 text-neutral-dark cursor-pointer hover:text-primary"
-          />
-        </div>
+    <div className="carousel relative w-full">
+      <div
+        className={`${
+          activeItem === 0 && "hidden"
+        } carousel__left-icon w-10 h-10 absolute flex justify-center items-center bg-neutral-grey-1 rounded-full left-5 sm:left-10 lg:left-40 top-28 lg:top-48 z-10`}
+        onClick={carouselLeftIconHandler}
+      >
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className="w-5 h-5 text-neutral-dark cursor-pointer hover:text-primary"
+        />
       </div>
+      <ul
+        className={`landing__car-preview-slides flex flex-row w-full mt-10 transition`}
+        style={{ transform: `translate(-${activeItem * 100}%, 0px)` }}
+      >
+        {items.map((item, index) => (
+          <li
+            key={item.id}
+            className={`carousel-item-${index} min-w-full flex flex-col items-center`}
+          >
+            <figure>
+              <img
+                src={item.imgSrc}
+                alt={item.alt}
+                className={`max-h-60 lg:h-96 lg:max-h-96 ${item.className}`}
+              />
+              <figcaption className="text-center text-neutral-grey-4">
+                {item.name}
+              </figcaption>
+            </figure>
+            {showDetails && (
+              <div className="bg-neutral-grey-1 flex flex-col w-2/3 gap-y-2 md:gap-y-0 items-start md:flex-row md:w-[560px] p-4 md:p-8 md:justify-between md:items-center mt-8">
+                <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
+                  <p className="text-neutral-grey-4 text-sub1">Category</p>
+                  <p className="text-neutral-dark">{item.details.category}</p>
+                </div>
+                <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
+                  <p className="text-neutral-grey-4 text-sub1">Mileage</p>
+                  <p className="text-neutral-dark">
+                    {item.details.mileage.toLocaleString()} kms
+                  </p>
+                </div>
+                <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
+                  <p className="text-neutral-grey-4 text-sub1">Fuel Type</p>
+                  <p className="text-neutral-dark">{item.details.fuelType}</p>
+                </div>
+                <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
+                  <p className="text-neutral-grey-4 text-sub1">Seats</p>
+                  <p className="text-neutral-dark">{item.details.seats}</p>
+                </div>
+                <div className="flex flex-row gap-x-12 justify-between w-full md:w-auto items-center md:flex-col md:items-center md:justify-center">
+                  <p className="text-neutral-grey-4 text-sub1">Price Per Day</p>
+                  <p className="text-neutral-dark">
+                    ${item.details.pricePerDay}
+                  </p>
+                </div>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+      {showBookButton && (
+        <div className="booking__book-button flex items-center justify-center mt-16">
+          <Button
+            className="min-w-[8rem] w-32 max-w-lg"
+            buttonStyle="primary"
+            type="button"
+            onClick={cartHandler}
+          >
+            {cartState.find(
+              (item: CartItemModel) => item.id === items[activeItem].id
+            )
+              ? "Added to Cart"
+              : "Book Now"}
+          </Button>
+        </div>
+      )}
+      <div
+        className={`${
+          activeItem === items.length - 1 && "hidden"
+        } carousel__right-icon w-10 h-10 absolute flex justify-center items-center bg-neutral-grey-1 rounded-full right-5 sm:right-10 lg:right-40 top-28 lg:top-48 z-10`}
+        onClick={carouselRightIconHandler}
+      >
+        <FontAwesomeIcon
+          icon={faChevronRight}
+          className="w-5 h-5 text-neutral-dark cursor-pointer hover:text-primary"
+        />
+      </div>
+    </div>
   );
 };
 
--- src/routes/booking/index.component.tsx
+++ src/routes/booking/index.component.tsx
@@ -12,17 +12,26 @@ const BookingPage: FC = () => {
 
   // fetch products from firestore
   useEffect(() => {
+    const lsProductState = localStorage.getItem('ls-product-state');
+
     const fetchProducts = async () => {
-      const db = getFirestore()
-      const q = query(collection(db, "products"));
-      const querySnapshot = await getDocs(q);
-      let products: CartItemModel[] = [];
+      if (!lsProductState) {
+        const db = getFirestore()
+        const q = query(collection(db, "products"));
+        const querySnapshot = await getDocs(q);
+        let products: CartItemModel[] = [];
 
-      querySnapshot.forEach((product: any) => {
-        return products.push(product.data())
-      });
+        querySnapshot.forEach((product: any) => {
+          return products.push(product.data())
+        });
 
-      setProductState(products)
+        setProductState(products)
+        localStorage.setItem('ls-product-state', JSON.stringify(products));
+
+        return;
+      }
+
+      setProductState(JSON.parse(lsProductState));
     }
 
     fetchProducts();
