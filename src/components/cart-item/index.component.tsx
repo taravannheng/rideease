b035a4c28982a84110010b8ddf6282463823624e
taravannheng <heng.t.taravann@gmail.com>
2023-01-02 14:38:09 +1100

    feat(component): prevent image from draggable

--- src/components/cart-item/index.component.tsx
+++ src/components/cart-item/index.component.tsx
@@ -17,7 +17,7 @@ const CartItem: FC<CartItemProps> = ({ item, className, onClick }) => {
     <li key={id} className={` w-full flex flex-row justify-between items-center ${className} min-h-[52px] md:min-h-[100px]`}>
       <div className=' basis-1/3 min-h-[52px] md:min-h-[100px]'>
         <div className='w-full flex justify-center'>
-          <img src={imgSrc} alt={alt} className="h-[52px] md:h-[100px]" />
+          <img src={imgSrc} alt={alt} className="h-[52px] md:h-[100px] pointer-events-none select-none" />
         </div>
       </div>
       <div className='basis-2/3 flex relative min-h-[52px] md:min-h-[100px] items-center'>
