e2715ab927bb2ca5ec74258b43c8c0a73ef80778
taravannheng <heng.t.taravann@gmail.com>
2023-01-03 13:04:07 +1100

    refactor(progress-indicator): remove animate presence

--- src/components/progress-indicator/index.component.tsx
+++ src/components/progress-indicator/index.component.tsx
@@ -1,26 +1,21 @@
 import { FC } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 import ImgLogoDark from "../../assets/logos/logo-dark.png";
 
 const ProgressIndicator: FC = () => {
   return (
-    <AnimatePresence>
-      <motion.div
-        exit={{ opacity: 0 }}
-        className="w-full h-screen flex flex-col gap-y-16 justify-center items-center"
-      >
-        <img src={ImgLogoDark} alt="site logo" className="h-16" />
-        <div className="progress-track w-[200px] h-[8px] bg-neutral-grey-1 p-[2px] overflow-hidden">
-          <motion.div
-            className="active-indicator w-[40px] h-[4px] bg-primary"
-            initial={{ translateX: 0 }}
-            animate={{ translateX: [0, 160, 0] }}
-            transition={{ duration: 2, ease: "easeInOut", repeat: Infinity }}
-          ></motion.div>
-        </div>
-      </motion.div>
-    </AnimatePresence>
+    <div className="w-full h-screen flex flex-col gap-y-16 justify-center items-center">
+      <img src={ImgLogoDark} alt="site logo" className="h-16" />
+      <div className="progress-track w-[200px] h-[8px] bg-neutral-grey-1 p-[2px] overflow-hidden">
+        <motion.div
+          className="active-indicator w-[40px] h-[4px] bg-primary"
+          initial={{ translateX: 0 }}
+          animate={{ translateX: [0, 160, 0] }}
+          transition={{ duration: 2, ease: "easeInOut", repeat: Infinity }}
+        ></motion.div>
+      </div>
+    </div>
   );
 };
 
