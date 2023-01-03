9a48a0278997c54a3ad2aceeb0c18447c0b50be7
taravannheng <heng.t.taravann@gmail.com>
2023-01-03 12:49:53 +1100

    feat(notfound): add onload to image to change loading state

--- src/routes/notfound/index.component.tsx
+++ src/routes/notfound/index.component.tsx
@@ -11,56 +11,48 @@ import ProgressIndicator from "../../components/progress-indicator/index.compone
 const NotFoundPage: FC = () => {
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <ProgressIndicator />
-      ) : (
-        <AnimatePresence>
-          <motion.div
-            initial={{ opacity: 0.7 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 1 }}
-            className="relative min-h-screen"
-          >
-            <Header type="landing" />
-            <main className="not-found mt-8 sm:20">
-              <div className="flex flex-row justify-center items-center gap-x-4">
-                <span className=" text-[96px] sm:text-[128px] font-bold text-primary">
-                  4
-                </span>
-                <img
-                  src={ImgTire}
-                  alt="car tire"
-                  className="w-[90px] h-[90px] sm:w-[120px] sm:h-[120px]"
-                />
-                <span className="text-[96px] sm:text-[128px] font-bold text-primary">
-                  4
-                </span>
-              </div>
-              <p className="text-center text-h3">Page Not Found</p>
-              <div className="flex justify-center">
-                <Button
-                  buttonStyle="primary"
-                  type="button"
-                  route={ROUTES.HOME}
-                  className="mt-12 min-w-[8rem] w-32 max-w-lg"
-                >
-                  Back to Home
-                </Button>
-              </div>
-            </main>
-            <Footer className="mt-20 md:w-full md:absolute md:bottom-0 md:right-0" />
-          </motion.div>
-        </AnimatePresence>
-      )}
+      {loading && <ProgressIndicator />}
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0.7 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="relative min-h-screen"
+        >
+          <Header type="landing" />
+          <main className="not-found mt-8 sm:20">
+            <div className="flex flex-row justify-center items-center gap-x-4">
+              <span className=" text-[96px] sm:text-[128px] font-bold text-primary">
+                4
+              </span>
+              <img
+                src={ImgTire}
+                alt="car tire"
+                className="w-[90px] h-[90px] sm:w-[120px] sm:h-[120px]"
+                onLoad={() => setLoading(false)}
+              />
+              <span className="text-[96px] sm:text-[128px] font-bold text-primary">
+                4
+              </span>
+            </div>
+            <p className="text-center text-h3">Page Not Found</p>
+            <div className="flex justify-center">
+              <Button
+                buttonStyle="primary"
+                type="button"
+                route={ROUTES.HOME}
+                className="mt-12 min-w-[8rem] w-32 max-w-lg"
+              >
+                Back to Home
+              </Button>
+            </div>
+          </main>
+          <Footer className="mt-20 md:w-full md:absolute md:bottom-0 md:right-0" />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
