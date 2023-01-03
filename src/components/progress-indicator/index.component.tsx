45ed0892c4b7a13882cce0242f57fc5bf4fe2ee8
taravannheng <heng.t.taravann@gmail.com>
2023-01-03 12:54:20 +1100

    feat(progress-indicator): add fade out effect

--- src/components/progress-indicator/index.component.tsx
+++ src/components/progress-indicator/index.component.tsx
@@ -1,21 +1,29 @@
 import { FC } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 import ImgLogoDark from "../../assets/logos/logo-dark.png";
 
 const ProgressIndicator: FC = () => {
   return (
-    <div className="w-full h-screen flex flex-col gap-y-16 justify-center items-center">
-      <img src={ImgLogoDark} alt="site logo" className="h-16" />
-      <div className="progress-track w-[200px] h-[8px] bg-neutral-grey-1 p-[2px]">
-        <motion.div
-          className="active-indicator w-[40px] h-[4px] bg-primary"
-          initial={{ translateX: 0 }}
-          animate={{ translateX: [0, 160, 0] }}
-          transition={{ duration: 2, ease: "easeInOut", repeat: Infinity }}
-        ></motion.div>
-      </div>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0.7 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full h-screen flex flex-col gap-y-16 justify-center items-center"
+      >
+        <img src={ImgLogoDark} alt="site logo" className="h-16" />
+        <div className="progress-track w-[200px] h-[8px] bg-neutral-grey-1 p-[2px]">
+          <motion.div
+            className="active-indicator w-[40px] h-[4px] bg-primary"
+            initial={{ translateX: 0 }}
+            animate={{ translateX: [0, 160, 0] }}
+            transition={{ duration: 2, ease: "easeInOut", repeat: Infinity }}
+          ></motion.div>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
