26b2ee897a0acd3bf8470a32fbe94b7cce5fa824
taravannheng <heng.t.taravann@gmail.com>
2023-01-04 14:17:09 +1100

    build(dependency): add husky and lint-staged to format before commit

--- src/components/accordion/index.component.tsx
+++ src/components/accordion/index.component.tsx
@@ -30,6 +30,7 @@ const Accordion: FC<AccordionProps> = ({ items, className }) => {
     <ul className={`accordion ${className}`}>
       {items.map((item, index) => (
         <motion.li
+
           layout
           transition={{
             opacity: { ease: "linear" },
@@ -39,10 +40,15 @@ const Accordion: FC<AccordionProps> = ({ items, className }) => {
           key={item.id}
           className={`mb-2 cursor-pointer overflow-hidden border-b-[1px] border-neutral-grey-2 p-2`}
         >
+
+
           <h4
             onClick={() => toggle(index)}
-            className="mb-2 flex flex-row items-center justify-between text-body font-medium text-neutral-dark hover:text-primary sm:text-h4"
+            className="mb-2 flex flex-row items-center  font-medium text-neutral-dark hover:text-primary sm:text-h4 justify-between text-body"
           >
+
+
+
             {item.title}{" "}
             <FontAwesomeIcon
               icon={expandedItem === index ? faChevronUp : faChevronDown}
